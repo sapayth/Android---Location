8f0de347272889aeee7aa751530f4ac97fd4d538
Sapayth <sapayth.qubee@gmail.com>
2019-05-17 12:39:44 +0600

    show location with latitude and longitude

--- app/src/main/java/com/example/accessfinelocaiton/MainActivity.java
+++ app/src/main/java/com/example/accessfinelocaiton/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.accessfinelocaiton;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
@@ -9,11 +11,16 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     private LocationCallback locationCallback;
     private LocationRequest locationRequest;
     TextView tvLocation;
+    TextView tvLocationStr;
     private boolean requestingLocationUpdate = true;
 
     @Override
@@ -30,6 +38,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tvLocation = findViewById(R.id.tv_location);
+        tvLocationStr = findViewById(R.id.tv_location_str);
 
         fusedLocationProviderClient = new FusedLocationProviderClient(this);
         locationCallback = new LocationCallback() {
@@ -42,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
                     Double lat = location.getLatitude();
                     Double lon = location.getLongitude();
                     tvLocation.setText("Lat: " + lat + ", Lon: " + lon);
+                    tvLocationStr.setText(getCompleteAddressString(lat, lon));
                 }
             }
         };
@@ -81,5 +91,27 @@ public class MainActivity extends AppCompatActivity {
                     fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null);
                 }
             }
+    }
+
+    private String getCompleteAddressString(double LATITUDE, double LONGITUDE) {
+        Geocoder geocoder;
+        List<Address> addresses = null;
+        geocoder = new Geocoder(this, Locale.getDefault());
+        String addr = "No Address Found";
+
+        try {
+            addresses = geocoder.getFromLocation(LATITUDE, LONGITUDE, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+            String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+            // String city = addresses.get(0).getLocality();
+            // String state = addresses.get(0).getAdminArea();
+            // String country = addresses.get(0).getCountryName();
+            // String postalCode = addresses.get(0).getPostalCode();
+            addr = address;
+        } catch (IOException ex) {
+            Toast.makeText(this, "Exception", Toast.LENGTH_SHORT);
+        }
+
+
+        return addr;
     }
 }
